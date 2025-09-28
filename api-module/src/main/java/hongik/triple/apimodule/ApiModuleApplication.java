b428efe3b441d637d1a6c7b252297ad33f02153b
dl-00-e8 <sso07012@gmail.com>
2025-09-29 00:38:34 +0900

    refactor: 미사용 import 제거

--- api-module/src/main/java/hongik/triple/apimodule/ApiModuleApplication.java
+++ api-module/src/main/java/hongik/triple/apimodule/ApiModuleApplication.java
@@ -6,8 +6,6 @@ import hongik.triple.inframodule.AcneLogInfraRoot;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Slf4j
 @SpringBootApplication(scanBasePackageClasses = {
